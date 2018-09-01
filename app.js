18aecdd60942ab72394bee8f1c97268ca19d8789
Pedro <pedro_r26@hotmail.com>
2018-09-01 21:25:34 +0200

    Modified app.listen to Heroku

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 // ]
 
 //Starting Server
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server Started");
 });
